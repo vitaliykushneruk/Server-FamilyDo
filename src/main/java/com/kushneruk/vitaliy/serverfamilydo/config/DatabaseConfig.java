05f05b16800d3dfdea86d425c55556c8913c6a5e
KUSHNERUK Vitaliy <Vitaliy.KUSHNERUK@partners.raiffeisen.ru>
2018-05-14 18:22:57 +0300

    add database config

---
+++ src/main/java/com/kushneruk/vitaliy/serverfamilydo/config/DatabaseConfig.java
@@ -0,0 +1,19 @@
+package com.kushneruk.vitaliy.serverfamilydo.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix="app.datasource")
+    DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
