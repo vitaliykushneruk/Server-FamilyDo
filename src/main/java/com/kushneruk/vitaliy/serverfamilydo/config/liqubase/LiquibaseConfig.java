957f216e9a24853aef31056e14f543ec3bb6ed0d
KUSHNERUK Vitaliy <Vitaliy.KUSHNERUK@partners.raiffeisen.ru>
2018-06-06 18:30:00 +0300

    #1 add support liqubase

---
+++ src/main/java/com/kushneruk/vitaliy/serverfamilydo/config/liqubase/LiquibaseConfig.java
@@ -0,0 +1,31 @@
+package com.kushneruk.vitaliy.serverfamilydo.config.liqubase;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LiquibaseConfig {
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.liquibase")
+    LiquibaseProperties liquibaseProperties(){
+        return new LiquibaseProperties();
+    }
+
+    @Bean
+    SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog(liquibaseProperties().getChangeLog());
+        liquibase.setDefaultSchema(liquibaseProperties().getDefaultSchema());
+        liquibase.setDataSource(dataSource);
+        return liquibase;
+    }
+}
