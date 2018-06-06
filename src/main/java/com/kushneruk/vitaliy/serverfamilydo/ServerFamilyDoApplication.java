3941a2a63590d341613d80517d1ac6f688505493
Виталий Кушнерук <vitaliy.kushneruk@gmail.com>
2018-06-06 23:05:45 +0300

    #3

--- src/main/java/com/kushneruk/vitaliy/serverfamilydo/ServerFamilyDoApplication.java
+++ src/main/java/com/kushneruk/vitaliy/serverfamilydo/ServerFamilyDoApplication.java
@@ -3,7 +3,9 @@ package com.kushneruk.vitaliy.serverfamilydo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
+@EnableJdbcRepositories
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class ServerFamilyDoApplication {
 
