051b9a54600dbae40eefd182f6bf12a7957d0eb1
KUSHNERUK Vitaliy <Vitaliy.KUSHNERUK@partners.raiffeisen.ru>
2018-06-07 11:07:11 +0300

    #4

---
+++ src/test/java/com/kushneruk/vitaliy/serverfamilydo/config/DatabaseConfig/DatabaseTestConfig.java
@@ -0,0 +1,20 @@
+package com.kushneruk.vitaliy.serverfamilydo.config.DatabaseConfig;
+
+import com.kushneruk.vitaliy.serverfamilydo.infrastructure.property.Profiles;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@TestConfiguration
+public class DatabaseTestConfig {
+    @Bean
+    @ConfigurationProperties(prefix="app.datasource")
+    @Profile(Profiles.TEST)
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
