660de3b0d0dddf245267e7f3d587452bace536cd
Виталий Кушнерук <vitaliy.kushneruk@gmail.com>
2018-07-21 13:25:00 +0300

    #5 edit tests validation controller

--- src/main/java/com/kushneruk/vitaliy/serverfamilydo/web/dto/UserDto.java
+++ src/main/java/com/kushneruk/vitaliy/serverfamilydo/web/dto/UserDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
@@ -15,21 +16,26 @@ import javax.validation.constraints.NotNull;
 public class UserDto {
 
     @NotNull
-//    @Size(min = 1,message = "{userDto.firstName.Size}")
+    @Size(min = 1, max = 255, message = "{userDto.firstName.Size}")
     private String firstName;
 
     @NotNull
-//    @Size(min = 1,message = "{userDto.firstName.Size}")
+    @Size(min = 1, max = 255, message = "{userDto.lastName.Size}")
     private String lastName;
 
     @NotNull
+    @Size(min = 1, max = 255, message = "{userDto.email.Size}")
     private String email;
 
     @NotNull
+    @Size(min = 1, max = 255, message = "{userDto.userName.Size}")
     private String userName;
 
     @NotNull
+    @Size(min = 1, max = 255, message = "{userDto.password.Size}")
     private String password;
+
     @NotNull
+    @Size(min = 1, max = 255, message = "{userDto.matchingPassword.Size}")
     private String matchingPassword;
 }
